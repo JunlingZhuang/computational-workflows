a3da4177d0ed8657f8eb2db2a7efe1164ee29414
JunlingZhuang <108850976+JunlingZhuang@users.noreply.github.com>
2022-07-18 18:43:55 -0400

    Update script.js

--- exercise_00/script.js
+++ exercise_00/script.js
@@ -1,1 +1,5 @@
+//window.alert("Hello World")
 console.log("This things work");
+let numberofstudents=18
+//“The number of students in this class is ${numberofstudeents}$”
+alert("The number of students in this class is ${numberofstudeents}$)"
